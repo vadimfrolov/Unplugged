773d7584367fcd7d5d1f0a990a64c3c924b70303
MikhailKolichko <mikhailkolichko@gmail.com>
2019-11-01 14:42:37 +0300

    Map w/o cluster

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -6,6 +6,7 @@ import "../App.css";
 import FBpanel from "./FBpanel";
 import Navbar from "./Navbar";
 import LandingPage from "./LandingPage";
+import Map from './Map';
 
 const App = () => (
    
@@ -16,6 +17,7 @@ const App = () => (
       <Route exact path="/" component={Home} />
       <Route path="/fbpanel" component={FBpanel} />
       <Route  path="/landing" component={LandingPage} />
+      <Route path="/map" component={Map} />
     </Switch>
     </Router>
   </div>
