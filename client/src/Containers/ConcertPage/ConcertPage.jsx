883fc6ecc0555c0de499b269226640d4d8031e22
Olga <olgashijan@rambler.ru>
2019-11-08 14:06:34 +0300

    done merge

--- client/src/Containers/ConcertPage/ConcertPage.jsx
+++ client/src/Containers/ConcertPage/ConcertPage.jsx
@@ -40,6 +40,7 @@ class ConcertPage extends Component {
   componentDidMount = async () => {
     const id = this.props.match.params.id;
     await this.props.fetchConcertInfoAC(id);
+    this.setState({isLoading:false})
   }
 
   onClick = async (e) => {
@@ -66,73 +67,80 @@ class ConcertPage extends Component {
     const comments = get(concertPage, "comments");
 
     return (
-      <Container style={{ marginTop: "40px", padding: "0px 30px", borderRadius: "3%" }}>
-        <Row>
-          <Col m={12} s={12}>
-            <Card
-              className="black"
-              textClassName="white-text"
-              title={name}
-            >
-              <Row>
-                <Col m={9}>
-                  <p style={{ fontSize: "35px", marginBottom: "25px" }} className="pointConcert" >
-                    {name}
-                  </p>
-                </Col>
-                <Col style={{ textAlign: "right" }} m={3}>
-                  {!this.props.user ?
-                    <></> :
-                    <GoButton concertPage={this.props.concertPage} user={this.props.user} />
-                  }
-                </Col>
-              </Row>
-              <p style={{ fontSize: "25px", marginBottom: "25px" }} className="pointConcert" >
-                <span style={{ fontWeight: "bold", fontSize: "35px", color: "#b71c1c", marginRight: "15px" }}>When:</span> {moment(new Date(date)).format("LL")}, {time}
-              </p>
-              <p style={{ fontSize: "25px", marginBottom: "25px" }}>
-                <span style={{ fontWeight: "bold", fontSize: "35px", color: "#b71c1c", marginRight: "15px" }}>Where:</span>
-                {venue}, {location}
-              </p>
-              <p style={{ fontSize: "25px", marginBottom: "25px" }}>
-                <span style={{ fontWeight: "bold", fontSize: "35px", color: "#b71c1c", marginRight: "15px" }}>Performers:</span>
-                {performers && performers.map((el, i) => (
-                  <Chip className="performersList" key={`${name}_${i}`}>
-                    <Link style={{ color: "black" }} to={`/artists/${performers[i].id}`} value={el.displayName} onClick={() => this.onClick(el.displayName)}>
-                      {el.displayName}
-                    </Link>
-                  </Chip>
-                ))}
-              </p>
+<>
+      {this.state.isLoading?<Spinner/>:
+      <>
+        <Container style={{ marginTop: "40px", padding: "0px 30px", borderRadius: "3%" }}>
+          <Row>
+            <Col m={12} s={12}>
+              <Card
+                className="black"
+                textClassName="white-text"
+                title={name}
+              >
+                <Row>
+                  <Col m={9}>
+                    <p style={{ fontSize: "35px", marginBottom: "25px" }} className="pointConcert" >
+                      {name}
+                    </p>
+                  </Col>
+                  <Col style={{ textAlign: "right" }} m={3}>
+                    {!this.props.user ?
+                      <></> :
+                      <GoButton concertPage={this.props.concertPage} user={this.props.user} />
+                    }
+                  </Col>
+                </Row>
+                <p style={{ fontSize: "25px", marginBottom: "25px" }} className="pointConcert" >
+                  <span style={{ fontWeight: "bold", fontSize: "35px", color: "#b71c1c", marginRight: "15px" }}>When:</span> {moment(new Date(date)).format("LL")}, {time}
+                </p>
+                <p style={{ fontSize: "25px", marginBottom: "25px" }}>
+                  <span style={{ fontWeight: "bold", fontSize: "35px", color: "#b71c1c", marginRight: "15px" }}>Where:</span>
+                  {venue}, {location}
+                </p>
+                <p style={{ fontSize: "25px", marginBottom: "25px" }}>
+                  <span style={{ fontWeight: "bold", fontSize: "35px", color: "#b71c1c", marginRight: "15px" }}>Performers:</span>
+                  {performers && performers.map((el, i) => (
+                    <Chip className="performersList" key={`${name}_${i}`}>
+                      <Link style={{ color: "black" }} to={`/artists/${performers[i].id}`} value={el.displayName} onClick={() => this.onClick(el.displayName)}>
+                        {el.displayName}
+                      </Link>
+                    </Chip>
+                  ))}
+                </p>
+                <>
+                {!this.props.user ? (
+                  <CommentList
+                  comments={comments}
+                  idConcert={id}
+                 
+                />
+                
+              ) : 
               <>
-              {!this.props.user ? (
-                <CommentList
-                comments={comments}
-                idConcert={id}
-               
-              />
-              
-            ) : 
-            <>
-            <CommentList
-            comments={comments}
-            idConcert={id}
-            idUser={this.props.user._id}
-          />
-          <CommentConcert nameArtist={performers} idConcert={id} />
+              <CommentList
+              comments={comments}
+              idConcert={id}
+              idUser={this.props.user._id}
+            />
+            <CommentConcert nameArtist={performers} idConcert={id} />
+            </>
+            }
+  
+      
           </>
-          }
-
-    
+  
+  
+                {/* <CommentConcert nameArtist={performers} idConcert={id} />
+                <CommentList comments={comments} /> */}
+              </Card>
+            </Col>
+          </Row>
+        </Container>
         </>
-
-
-              {/* <CommentConcert nameArtist={performers} idConcert={id} />
-              <CommentList comments={comments} /> */}
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+      
+      }
+</>
     );
   }
 }
